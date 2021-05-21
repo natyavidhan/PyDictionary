fed771234e047abefbae2c14b9a937a6acb219fa
Studious Gamer <67009562+studiousgamer@users.noreply.github.com>
2021-05-21 22:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,59 +2,60 @@ import PySimpleGUI as sg
 from PyDictionary import PyDictionary
 
 
-# ===============================================================================================
+# =================================================GUI==============================================
 
 
 layout = [
     [
-     sg.Text(size=(26, 0)), 
-     sg.Text("PyDictionary",font='Acme 60'), 
-     sg.Text(size=(20, 0))
-     ],
-    
-    [ 
-     sg.Text('_' * 110, justification='c')
-     ],
-    
-    [
-     sg.Text(size=(20, 3))
+        sg.Text(size=(26, 0)),
+        sg.Text("PyDictionary", font='Acme 60'),
+        sg.Text(size=(20, 0))
     ],
-    
+
     [
-     sg.Text(size=(5, 1)), 
-     sg.Text('Word-Meaning', font='Acme 12', size=(15, 1)), 
-     sg.Text(size=(15, 1)), 
-     sg.Text('Antonym', font='Acme 12', size=(15, 1)), 
-     sg.Text(size=(15, 1)), 
-     sg.Text('Synonym', font='Acme 12', size=(15, 1))
-     ],
-    
-    [ 
-     sg.Text(size=(5, 1)), 
-     sg.Input(font='Acme 12', size=(15, 1), key="meaning"), 
-     sg.Text(size=(13, 1)), 
-     sg.Input(font='Acme 12', size=(15, 1), key="antonym"), 
-     sg.Text(size=(13, 1)), 
-     sg.Input(font='Acme 12', size=(15, 1), key="synonym")
-     ],
-    
-    [ 
-     sg.Text(size=(5, 1)), 
-     sg.Button('Search', font='Acme 12', size=(15, 1), key="meaningsearch"), 
-     sg.Text(size=(15, 1)), 
-     sg.Button('Search', font='Acme 12', size=(15, 1), key="antonymmsearch"), 
-     sg.Text(size=(15, 1)), 
-     sg.Button('Search', font='Acme 12', size=(15, 1), key="synonymsearch")
-     ],
-    
-    [ 
-     sg.Text(size=(5, 1)), 
-     sg.Multiline(font='Acme 12', size=(20, 5), key="meaningres"), 
-     sg.Text(size=(5, 1)), 
-     sg.Multiline(font='Acme 12', size=(20, 5), key="antonymres"), 
-     sg.Text(size=(6, 1)), 
-     sg.Multiline(font='Acme 12', size=(20, 5), key="synonymres")
-     ]
+        sg.Text('_' * 110, justification='c')
+    ],
+
+    [
+        sg.Text(size=(20, 3))
+    ],
+
+    [
+        sg.Text(size=(5, 1)),
+        sg.Text('Word-Meaning', font='Acme 12', size=(15, 1)),
+        sg.Text(size=(15, 1)),
+        sg.Text('Antonym', font='Acme 12', size=(15, 1)),
+        sg.Text(size=(15, 1)),
+        sg.Text('Synonym', font='Acme 12', size=(15, 1))
+    ],
+
+    [
+        sg.Text(size=(5, 1)),
+        sg.Input(font='Acme 12', size=(15, 1), key="meaning"),
+        sg.Text(size=(13, 1)),
+        sg.Input(font='Acme 12', size=(15, 1), key="antonym"),
+        sg.Text(size=(13, 1)),
+        sg.Input(font='Acme 12', size=(15, 1), key="synonym")
+    ],
+
+    [
+        sg.Text(size=(5, 1)),
+        sg.Button('Search', font='Acme 12', size=(15, 1), key="meaningsearch"),
+        sg.Text(size=(15, 1)),
+        sg.Button('Search', font='Acme 12', size=(
+            15, 1), key="antonymmsearch"),
+        sg.Text(size=(15, 1)),
+        sg.Button('Search', font='Acme 12', size=(15, 1), key="synonymsearch")
+    ],
+
+    [
+        sg.Text(size=(5, 1)),
+        sg.Multiline(font='Acme 12', size=(20, 5), key="meaningres"),
+        sg.Text(size=(5, 1)),
+        sg.Multiline(font='Acme 12', size=(20, 5), key="antonymres"),
+        sg.Text(size=(6, 1)),
+        sg.Multiline(font='Acme 12', size=(20, 5), key="synonymres")
+    ]
 ]
 
 
@@ -100,8 +101,8 @@ def sort(meaning):
     except:
         pass
     return final
-    
-                 
+
+
 window = sg.Window('PyDictionary', layout, icon='book.ico')
 while True:
     event, values = window.read()
@@ -110,8 +111,8 @@ while True:
     elif event == 'meaningsearch':
         meaning = sort(PyDictionary.meaning(values['meaning']))
         window['meaningres'].update(meaning)
-        
-    elif event == 'synonymsearch':        
+
+    elif event == 'synonymsearch':
         synonym = PyDictionary.synonym(values['synonym'])
         finalsynonyms = ''
         num = 0
@@ -122,8 +123,8 @@ while True:
             else:
                 pass
         window['synonymres'].update(finalsynonyms)
-        
-    elif event == 'antonymmsearch':   
+
+    elif event == 'antonymmsearch':
         antonym = PyDictionary.antonym(values['antonym'])
         finallantonyms = ''
         num = 0
@@ -132,7 +133,7 @@ while True:
                 num += 1
                 finallantonyms += f'{num}: {ant}\n'
         else:
-            pass     
+            pass
         window['antonymres'].update(finallantonyms)
-        
+
 window.close()
